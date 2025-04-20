1eab4cc771dbad2730551708e3f295b6aad2d99d
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2025-04-20 12:59:25 +0100

    fix(codebase): head -> slot renaming in test

--- nori/tests/benchmark_sha256_serde.rs
+++ nori/tests/benchmark_sha256_serde.rs
@@ -1,6 +1,6 @@
 use alloy_primitives::FixedBytes;
 use anyhow::Result;
-use nori::{helios::get_latest_finality_head_and_store_hash, proof_outputs_decoder::DecodedProofOutputs, sp1_prover::{prepare_zk_program_input, ELF}};
+use nori::{helios::get_latest_finality_slot_and_store_hash, proof_outputs_decoder::DecodedProofOutputs, sp1_prover::{prepare_zk_program_input, ELF}};
 use sp1_sdk::{ProverClient, SP1PublicValues, SP1Stdin};
 
 /// A stripped down version of finality_update_job used to generate cycle information without the opt
@@ -49,26 +49,26 @@ pub async fn benchmark_finality_update(
 async fn benchmark_sha256_serde() {
     dotenv::dotenv().ok();
 
-    // Get latest head and store_hash
-    let (current_head, store_hash) = get_latest_finality_head_and_store_hash().await.unwrap();
+    // Get latest heaslotd and store_hash
+    let (current_slot, store_hash) = get_latest_finality_slot_and_store_hash().await.unwrap();
 
     // Perform benchmark initial
     println!("Round one sin opt");
-    let public_values = benchmark_finality_update(current_head, store_hash).await.unwrap();
+    let public_values = benchmark_finality_update(current_slot, store_hash).await.unwrap();
 
     // Decode public output
     let public_values_bytes = public_values.as_slice(); 
     let proof_outputs = DecodedProofOutputs::from_abi(public_values_bytes).unwrap();
-    let new_head = proof_outputs.new_head;
-    let new_head_bytes: [u8; 32] = new_head.to_be_bytes();
+    let new_slot = proof_outputs.new_head;
+    let new_slot_bytes: [u8; 32] = new_slot.to_be_bytes();
     // Extract the last 8 bytes (least significant bytes in big-endian)
-    let new_head_u64_bytes: [u8; 8] = new_head_bytes[24..32]
+    let new_slot_u64_bytes: [u8; 8] = new_slot_bytes[24..32]
         .try_into()
         .map_err(|_| anyhow::anyhow!("Failed to extract u64 bytes")).unwrap();
     // Convert the bytes to u64 using big-endian interpretation
-    let output_head = u64::from_be_bytes(new_head_u64_bytes);
+    let output_slot = u64::from_be_bytes(new_slot_u64_bytes);
 
     // Perform benchmark 2nd round
     println!("Round two with opt");
-    benchmark_finality_update(output_head, proof_outputs.store_hash).await.unwrap();
+    benchmark_finality_update(output_slot, proof_outputs.store_hash).await.unwrap();
 }
