d866d952306ac97ce5ec9320771cc7e27a76da74
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-01-14 07:57:57 +0000

    Remove binding generated file which was overwritten!

--- nori/src/contract_bindings/src/lib.rs
+++ nori/src/contract_bindings/src/lib.rs
@@ -1,7 +1,1 @@
-use alloy::sol;
-sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    NoriStateBridge,
-    "/home/jonathan/code/nori/nori-bridge-head/nori/src/contract_bindings/node_modules/@nori-zk/ethereum-token-bridge/build/artifacts/contracts/NoriTokenBridge.sol/NoriTokenBridge.json"
-);
+// @generated: build.rs will overwrite this with alloy::sol! bindings.
